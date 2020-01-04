1d6b7c7250882e8dd4f3ec76741d57de242459b2
Joshua Coleman <procoleman77@outlook.com>
2020-01-04 18:27:05 -0500

    Updates

---
+++ precache-manifest.bd66354452e0f860b6261aee31cf4f96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c402f49665f9a09767bc42a18094f48d",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "aad7044aef4adcdf1372",
+    "url": "/Click-Game/static/css/main.47b5a663.chunk.css"
+  },
+  {
+    "revision": "4212acc56508b9b22dc0",
+    "url": "/Click-Game/static/js/2.29e2b810.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Click-Game/static/js/2.29e2b810.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aad7044aef4adcdf1372",
+    "url": "/Click-Game/static/js/main.a0567cb9.chunk.js"
+  },
+  {
+    "revision": "7e00eb9b90f508620f4b",
+    "url": "/Click-Game/static/js/runtime-main.c6dade65.js"
+  },
+  {
+    "revision": "fcd28f03325ff404633f80baf5805236",
+    "url": "/Click-Game/static/media/17DA3EE7-608C-499B-B8C9-E46410236148_4_5005_c.fcd28f03.jpeg"
+  },
+  {
+    "revision": "e7925c762f545771eecc5df5e6e6ef32",
+    "url": "/Click-Game/static/media/6EA0E064-4DCD-41D8-93D9-5A14B437E639_4_5005_c.e7925c76.jpeg"
+  },
+  {
+    "revision": "f617ec3c5566cfdcdd4f9d94d4ff7507",
+    "url": "/Click-Game/static/media/9B584571-8956-41F2-9078-9EB122AFF51D_4_5005_c.f617ec3c.jpeg"
+  },
+  {
+    "revision": "c7ea2d1e8c9c259715d8fc35b7acff89",
+    "url": "/Click-Game/static/media/D7A9F136-8F39-4AB4-90D7-4B7FB3C8CF68_4_5005_c.c7ea2d1e.jpeg"
+  }
+]);
